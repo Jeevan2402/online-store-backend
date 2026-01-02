70a087c68460fce3002ad130fc36a4bb4f349dca
Jeevan2402 <74665644+Jeevan2402@users.noreply.github.com>
2026-01-03 00:02:39 +0530

    product initial

---
+++ services/product/src/main/java/com/store/product/entity/Category.java
@@ -0,0 +1,34 @@
+package com.store.product.entity;
+
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String description;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+	private List<Product> product;
+}
